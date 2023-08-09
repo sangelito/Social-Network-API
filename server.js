c5702fb07404147ed464c6c505ba9731b4a14d08
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-09 12:48:51 -0500

    connect mongoose

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
