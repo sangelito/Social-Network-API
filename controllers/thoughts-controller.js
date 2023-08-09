7a562db69acff09f63ba68b137e53c30005aeef0
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-09 13:44:31 -0500

    new thought

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -1,1 +1,18 @@
 const {Thoughts, Users} = require('../models');
+
+const thoughtsController = {
+    createThoughts({params, body}, res) {
+        Thoughts.create(body)
+        .then(({_id}) => {
+            return Users.findOneAndUpdate({ _id: params.userId}, {$push: {thoughts: _id}}, {new: true});
+        })
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+                res.status(404).json({message: 'No thoughts with this particular ID!'});
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch(err => res.json(err)); 
+    },
+}
