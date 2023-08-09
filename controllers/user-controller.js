fb5be0e868917841078e823036c1d7492e58b984
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-09 14:34:53 -0500

    create new user and get all users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,23 @@
 const {Users} = require('../models');
+
+const userController = {
+    createUsers({body}, res) {
+        Users.create(body)
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => res.status(400).json(err));
+    },
+    getAllUsers(req, res) {
+        Users.find({})
+        // populate users thoughts
+        .populate({path: 'thoughts', select: '-__v'})
+        // populate user friends
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        // .sort({_id: -1})
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        }); 
+    },
+}
