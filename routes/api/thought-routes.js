4c53cba4cddc944a405c490612e5555bb57b5ae2
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-09 14:58:57 -0500

    requirements from controller

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,1 +1,11 @@
 const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtsById, 
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thoughts-controller')
