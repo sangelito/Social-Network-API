b66742b606b416086c91acfa1c76eae4232278cf
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-09 15:01:37 -0500

    thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -8,4 +8,16 @@ const {
     deleteThoughts,
     addReaction,
     deleteReaction,
-} = require('../../controllers/thoughts-controller')
+} = require('../../controllers/thoughts-controller');
+
+router.route('/').get(getAllThoughts);
+
+router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts); 
+
+router.route('/:userId').post(createThoughts);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
