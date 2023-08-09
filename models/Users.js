cadfa59b7de9b065ea64776a1314fd51f39a6636
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-09 13:11:27 -0500

    User Schema

--- models/Users.js
+++ models/Users.js
@@ -1,1 +1,32 @@
 const { Schema, model } = require('mongoose');
+
+const UserSchema = new schema (
+    {
+        username:{
+            type: String, 
+            unique: true,
+            required: true, 
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Users'
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false
+        }
+)
