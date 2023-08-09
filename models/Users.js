3fc323fddcc7c5126ffdf97d1cfa1e7b91d870d2
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-09 16:38:09 -0500

    regex

--- models/Users.js
+++ models/Users.js
@@ -12,6 +12,7 @@ const UserSchema = new schema (
             type: String,
             required: true,
             unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
         },
         thoughts: [{
             type: Schema.Types.ObjectId,
